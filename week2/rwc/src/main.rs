2bbaacca8d61f2c384c407ea63fd0bf27e489037
zhaochanglong <changlong.zhao@outlook.com>
2026-05-21 00:20:04 +0800

    Add line, word, and character count functions

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,5 +1,26 @@
 use std::env;
+use std::fs::File; // For read_file_lines()
+use std::io::{self, BufRead, Read};
 use std::process;
+
+fn lines_count(filename: &str) -> Result<usize, io::Error> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines().count())
+}
+
+fn words_count(filename: &str) -> Result<usize, io::Error> {
+    let file = File::open(filename)?;
+    let mut buf = String::new();
+    io::BufReader::new(file).read_to_string(&mut buf)?;
+    Ok(buf.split_whitespace().count())
+}
+
+fn chars_count(filename: &str) -> Result<usize, io::Error> {
+    let file = File::open(filename)?;
+    let mut buf = String::new();
+    io::BufReader::new(file).read_to_string(&mut buf)?;
+    Ok(buf.chars().count())
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,5 +29,8 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+
+    println!("words count: {}", words_count(filename).unwrap());
+    println!("lines count: {}", lines_count(filename).unwrap());
+    println!("chars count: {}", chars_count(filename).unwrap());
 }
